2c698c7fb9c2baab5a024b8cfe4001e6c277e330
Anirudh Jain <cynide@Anirudhs-MacBook-Pro.local>
2018-11-10 11:09:43 +0530

    Adding Unity Projects

---
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    int max;
+    int min;
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        Debug.Log("Hello, Welcome to Number Wizard");
+        Debug.Log("Pick a number between " + max + " and " + min + ", dont tell me what it is");
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Push up = Higher, Push down = Lower, Push enter = correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("You've got it correct");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("Is it higher or lower than " + guess);
+    }
+}
