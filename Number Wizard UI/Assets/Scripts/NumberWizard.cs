5db6ff4a2481572ae1160ff2090163e3059820d1
Anirudh Jain <cynide@Anirudhs-MacBook-Pro.local>
2018-11-11 00:24:19 +0530

    Started working on Block Breaker

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Use this for initialization
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame() {
+        generateRandomNumber(min, max);
+        guessText.text = guess.ToString();
+        max = max + 1;
+    }
+
+    public void OnPressHigher() {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower() {
+        max = guess;
+        NextGuess();
+    }
+
+    private void NextGuess()
+    {
+        generateRandomNumber(min, max);
+        guessText.text = guess.ToString();
+    }
+
+    private void generateRandomNumber(int lowerLimit, int upperLimit) {
+        int newGuess = Random.Range(lowerLimit, upperLimit + 1);
+        if (newGuess == guess) {
+            newGuess = Random.Range(lowerLimit, upperLimit + 1);
+        }
+        guess = newGuess;
+    }
+}
